1e94b8f376d89cfa527083bf7c5cf1d9284ea204
Giang Nhật Khánh <73809007+NhatKhanhGiang@users.noreply.github.com>
2021-06-01 12:47:56 +0700

    added CMS copyright feature

--- Registration.php
+++ Registration.php
@@ -550,7 +550,12 @@ if (isset($_POST['register'])) {
         <!--GItem 3-->
       </div>
       <div class="copyright">
-        <p>&copy; 2021 The Infinis Mall. All Rights Reserved.</p>
+      <?php 
+            $terms_file = fopen("data/copyright.txt", "r");
+            while ($line = fgets($terms_file))
+                echo $line;
+            fclose($terms_file);
+      ?>
       </div>
     </footer>
   </div>
